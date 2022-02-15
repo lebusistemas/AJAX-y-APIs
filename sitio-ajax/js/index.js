8b03e642b0c3acd06f49ff83da1e46f0a89be50c
Leandro Espinoza <leandroespinoza@gmail.com>
2022-02-15 17:54:52 -0400

    incluir html ajax

--- sitio-ajax/js/index.js
+++ sitio-ajax/js/index.js
@@ -1,42 +1,41 @@
-/* **********     Curso JavaScript: 122. Ejercicios AJAX - APIs: Sitio web con AJAX - #jonmircha     ********** */
 const d = document,
-$main = d.querySelector("main");
+  $main = d.querySelector("main");
 
 const getHTML = (options) => {
-let { url, success, error } = options;
-const xhr = new XMLHttpRequest();
+  let { url, success, error } = options;
+  const xhr = new XMLHttpRequest();
 
-xhr.addEventListener("readystatechange", (e) => {
-  if (xhr.readyState !== 4) return;
-  if (xhr.status >= 200 && xhr.status < 300){
-    let html = xhr.responseText;
-    success(html);
-  } else {
-    let message = xhr.statusText || "Ocurrió un error";
-    error(`Error ${xhr.status}: ${message}`);
-  }
-});
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let html = xhr.responseText;
+      success(html);
+    } else {
+      let message = xhr.statusText || "Ocurrió un error";
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
 
-xhr.open("GET", url);
-xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
-xhr.send();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+  xhr.send();
 };
 
 d.addEventListener("DOMContentLoaded", (e) => {
-getHTML({
-  url: "assets/home.html",
-  success: html => $main.innerHTML = html,
-  error: (err)=> $main.innerHTML = `<h1>${err}</h1>`
-});
+  getHTML({
+    url: "assets/home.html",
+    success: html => $main.innerHTML = html,
+    error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+  });
 });
 
 d.addEventListener("click", (e) => {
-if (e.target.matches(".menu a")){
-  e.preventDefault();
-  getHTML({
-    url: e.target.href,
-    success: html => $main.innerHTML = html,
-    error: (err)=> $main.innerHTML = `<h1>${err}</h1>`
-  });
-}
+  if (e.target.matches(".menu a")) {
+    e.preventDefault();
+    getHTML({
+      url: e.target.href,
+      success: html => $main.innerHTML = html,
+      error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+    });
+  }
 });
